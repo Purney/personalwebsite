f98a3f582f30cf5ac844ff9a23d27b9507e6e822
Will Purnell <william.purnell2016@gmail.com>
2025-01-05 12:01:18 +0000

    Add reply body content

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -2,18 +2,23 @@ import { sendEmail } from "@/lib/mailgun";
 
 export async function POST(request) {
     let { name, email, service, otherService, message } = await request.json();
-
+    console.log(service);
     if (!service || service == "Other") {
+        console.log("it is other");
         service = otherService;
+        console.log(service);
     }
 
-    var subject = `NEW ${service} REQUEST from ${name}`
+    var subject = `NEW ${service} REQUEST from ${name}`;
+
+    var replyBodyContent = `Hi ${name}, thanks for contacting us in regards to ${service}, in order to understand how we can best help you we'd love to have a discovery call, you can book a meeting with us here #enterbookingmeeting#` 
+
     var emailText = `
     <html>
     <h1>${subject}</h1>
     <h2>Service: ${service}</h2>
     <p>${message}</p>
-    <a href="mailto:${email}">Reply to ${email} </a>
+    <a href="mailto:${email}?subject=RE-${encodeURIComponent(service)}&body=${encodeURIComponent(replyBodyContent)}">Reply to ${email} </a>
     </html>
     `
 
