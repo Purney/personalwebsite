55cac6db6c8da074c34880423068b629e2623f45
Will Purnell <william.purnell2016@gmail.com>
2025-01-20 21:22:47 +0000

    Add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,52 @@
+import { Data } from "@/data/projectsData";
+
+export default async function sitemap() {
+  // Base URL - replace with your actual domain
+  const baseUrl = 'https://localhost:3000';
+
+  // Static routes
+  const staticRoutes = [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+        url: `${baseUrl}/services`,
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: `${baseUrl}/projects`,
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.6,
+    }
+  ]
+
+  // Dynamic routes (blog posts)
+  const projects = await Data;
+  const dynamicRoutes = projects.map(post => ({
+    url: `${baseUrl}/projects/${post.id}`,
+    lastModified: post.dateUpdated ?? post.dateCreated,
+    changeFrequency: 'weekly',
+    priority: 0.7,
+  }))
+
+  // Combine static and dynamic routes
+  return [...staticRoutes, ...dynamicRoutes]
+}
