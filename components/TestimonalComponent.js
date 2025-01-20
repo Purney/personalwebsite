e6be9727e1cb9ee5eeeac6d76716094575216bde
Will Purnell <william.purnell2016@gmail.com>
2025-01-20 20:28:50 +0000

    Add testimonials

--- components/TestimonalComponent.js
+++ components/TestimonalComponent.js
@@ -11,21 +11,22 @@ export default function TestimonalComponent() {
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:px-28 gap-8">
           {testimonials.map((testimonial, index) => (
             <div
-              className="card shadow-sm p-6 border-gray-200 border-2"
+              className="card shadow-sm p-6 border-gray-200 border-2 flex flex-col gap-8 justify-between"
               key={"testimonial-" + index}
             >
-              <blockquote className="pb-12 text-sm text-left">&quot;{testimonial.quote}&quot;</blockquote>
-              <div className="bg-gray-200 flex justify-center">
-                <div className="flex justify-items-center gap-3 px-5 py-5">
+              <q className="text-sm text-left">{testimonial.quote}</q>
+              <div className="bg-gray-200 flex md:flex-row flex-col justify-center">
+                <div className="flex justify-items-center gap-3 px-5 py-5 items-center">
                   <Image
                     className="w-16 h-16 object-cover rounded-full"
                     src={testimonial.image}
                     alt={testimonial.name + " picture"}
                   />
 
-                  <div className="text-left content-center"> 
-                    <p className="font-bold text-xs md:text-xl">{testimonial.name}</p>
-                    <p className="text-xxs md:text-sm">{testimonial.role}</p>
+                  <div className="text-left content-center "> 
+                    <p className="font-bold text-xs md:text-sm">{testimonial.name}</p>
+                    <p className="text-xxs md:text-xs">{testimonial.role}</p>
+                    <p className="text-xxs md:text-xs font-bold">{testimonial.company}</p>
                   </div>
                 </div>
               </div>
--- data/testimonialData.js
+++ data/testimonialData.js
@@ -1,26 +1,32 @@
-import placeholder from "../assets/images/placeholder.jpg";
 import georgeElliot from "../assets/images/testimonials/georgeElliot.jpg"
+import tomSearle from "../assets/images/testimonials/tomSearle.jpg"
+import benRobb from "../assets/images/testimonials/benRobb.jpg"
+
+
 
 export const Data = [
     {
-        quote: "Lorem ipsum odor amet, consectetuer adipiscing elit. Euismod fringilla tortor neque eu interdum rutrum netus. Sit luctus",
+        quote: "It's been a smooth experience creating my custom 3D printing website with Will. He's always interested in developing the best user experience for my customers.",
         name: "Ben Robb",
-        role: "CEO of FilamentGifts",
-        image: placeholder,
+        role: "CEO",
+        company: "FilamentGifts",
+        image: benRobb,
         link: "https://filamentgifts.com/"
     },
     {
         quote: "Will has a great hardworking attitude and is easy to get on with. Looking forward to working with him again",
         name: "George Elliot",
-        role: "Software Engineer at ASOS",
+        role: "Software Engineer",
+        company: "ASOS",
         image: georgeElliot,
         link: "https://www.linkedin.com/in/george-elliott-b6696386/"
     },
     {
-        quote: "Lorem ipsum odor amet, consectetuer adipiscing elit. Euismod fringilla tortor neque eu interdum rutrum netus. Sit luctus",
-        name: "Author Name",
-        role: "VP of Company",
-        image: placeholder,
-        link: "www.google.com"
+        quote: "Will is a dedicated developer with a fantastic ability to pick up new software and concepts.",
+        name: "Tom Searle",
+        role: "Lead Software Engineer",
+        company: "AML Analytics",
+        image: tomSearle,
+        link: "https://www.linkedin.com/in/twsearle/"
     },
 ]
