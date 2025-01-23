4909774d347bceae992f1da38773e2dbcbc726c0
Will Purnell <william.purnell2016@gmail.com>
2025-01-23 19:22:39 +0000

    Update config details

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const config = {
       subdomain: 'mg',                 // Optional subdomain if used in Mailgun (can be empty or undefined)
     },
     domainName: 'william-purnell.com',    // Your Mailgun domain (e.g., your Mailgun domain)
-    appName: "William Purnell",
-    appDescription: "Software Developer from Bournemouth for Hire"
+    appName: "William Purnell - Freelance Software Developer from Bournemouth",
+    appDescription: "Will is a software developer for Hire with 5 years experience based in Bournemouth"
   };
 
   
