0bdc364c407e8f110609ab6fe356dc01771487b1
Will Purnell <william.purnell2016@gmail.com>
2025-02-03 17:56:01 +0000

    Remove George

--- data/testimonialData.js
+++ data/testimonialData.js
@@ -1,4 +1,3 @@
-import georgeElliot from "../assets/images/testimonials/georgeElliot.jpg"
 import tomSearle from "../assets/images/testimonials/tomSearle.jpg"
 import benRobb from "../assets/images/testimonials/benRobb.jpg"
 
@@ -12,14 +11,6 @@ export const Data = [
         company: "FilamentGifts",
         image: benRobb,
         link: "https://filamentgifts.com/"
-    },
-    {
-        quote: "Will has a great hardworking attitude and is easy to get on with. Looking forward to working with him again",
-        name: "George Elliot",
-        role: "Software Engineer",
-        company: "ASOS",
-        image: georgeElliot,
-        link: "https://www.linkedin.com/in/george-elliott-b6696386/"
     },
     {
         quote: "Will is a dedicated developer with a fantastic ability to pick up new software and concepts.",
