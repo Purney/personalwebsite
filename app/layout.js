dedbb60221073acb864a4f920b9b977ef8b5558c
Will Purnell <william.purnell2016@gmail.com>
2025-08-06 21:32:27 +0100

    Make script async

--- app/layout.js
+++ app/layout.js
@@ -27,10 +27,10 @@ export default function RootLayout({ children }) {
       <head>
         {/* Google Analytics */}
         <Script
+          async
           src="https://www.googletagmanager.com/gtag/js?id=G-8KKTX6PF0"
-          strategy="afterInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
