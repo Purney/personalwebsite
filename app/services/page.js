0f4c57daaac1549f0d57fba5cf3483aabf3688ad
Will Purnell <william.purnell2016@gmail.com>
2025-11-06 19:40:26 +0000

    Update services page

--- app/services/page.js
+++ app/services/page.js
@@ -7,7 +7,6 @@ import { getSEOTags, getServicesSchema } from "@/lib/seo";
 export const metadata = getSEOTags({title: "William Purnell - Professional Software Development Services in Bournemouth", description: "Looking for expert software development services in Bournemouth? You’re in the right place. With years of experience in .NET, React, and AI technologies, I provide end-to-end solutions that help businesses thrive in today’s digital landscape."});
 
 export default async function Services() {
-  const services = await Data;
   const reasons = await HireReasonsData;
 
   return (
@@ -19,9 +18,11 @@ export default async function Services() {
           description="<h2>Looking for expert software development services in Bournemouth? </h2>
           You're in the right place! <br> "
         />
-        <div className="max-w-7xl mx-auto">
-          <ShowcaseComponent title="Our Services" services={services} />
-        </div>
+         {Data.length > 0 && (
+          <div className="max-w-7xl mx-auto">
+            <ShowcaseComponent title="Our Services" services={Data} />
+          </div>
+         )}
         <div className="bg-dark-section text-cta-text px-8 py-16 flex justify-center">
           <div className="max-w-7xl">
             <h2 className="text-2xl font-bold text-center  mb-8">
